0484c0ec9ac99532cf33f322417274991cc77c67
Andy <ajscottlewis@gmail.com>
2023-03-24 11:11:27 +0000

    Add definitions of polymorphism, method overloading and method overriding

--- polymorphism/methodOverloading.java
+++ polymorphism/methodOverloading.java
@@ -1,2 +1,11 @@
 package polymorphism;
 
+// Polymorphism refers to the ability of objects to take on multiple forms or
+// behaviors. In OOP, polymorphism is achieved through method overloading and
+// method overriding.
+
+// Method overloading allows a class to have multiple methods with the same
+// name, but different parameters. This enables a single method name to be used
+// for multiple tasks, which makes code more readable and reduces complexity.
+// Example of method overloading:
+
