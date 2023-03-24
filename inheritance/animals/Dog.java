f2f651242d5142f61ced295ffcfb8e708452b5ee
Andy <ajscottlewis@gmail.com>
2023-03-24 12:11:44 +0000

    Update package name to include subfolder

--- inheritance/animals/Dog.java
+++ inheritance/animals/Dog.java
@@ -1,5 +1,12 @@
 package inheritance.animals;
 
+// INHERITANCE
+
+// Inheritance refers to the ability of a subclass to inherit the properties and
+// methods of its superclass. This allows for code reuse and can help to reduce
+// redundancy in a program.
+// Below is an example of this
+
 class Dog extends Animal {
     public Dog() {
         super("dog");
