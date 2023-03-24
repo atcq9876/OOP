a9d6e62198ecbaff0f5541263855ae31789f2406
Andy <ajscottlewis@gmail.com>
2023-03-24 12:14:44 +0000

    Update example explanation

--- inheritance/animals/Animal.java
+++ inheritance/animals/Animal.java
@@ -5,7 +5,9 @@ package inheritance.animals;
 // Inheritance refers to the ability of a subclass to inherit the properties and
 // methods of its superclass. This allows for code reuse and can help to reduce
 // redundancy in a program.
-// Below is an example of this
+// Below is an example of this. In this example, the Animal class defines the
+// basic properties and behaviors of all animals, which the Dog class inherits
+// but also adds additional properties and behaviors specific to dogs.
 
 class Animal {
     protected String species;
