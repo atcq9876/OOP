3b4a7f50d2460df1758d1b9f864d4424e42bbff9
Andy <ajscottlewis@gmail.com>
2023-03-27 12:45:43 +0100

    Add explanation and examples of abstraction

---
+++ abstraction/Abstraction.java
@@ -0,0 +1,42 @@
+// ABSTRACTION
+
+// Abstraction in object-oriented programming is the process of simplifying
+// complex systems by breaking them down into more manageable and understandable
+// components. It allows programmers to focus on important aspects of a system,
+// while hiding unnecessary details.
+// - Programming example: if I am working in the frontend and I want to get
+// information from a database, I just make a fetch request to that backend
+// which retrieves and returns that information to me - I don't need to know how
+// the API does this, just that it will return that information to me.
+
+// In terms of an end user of a website, abstraction in object-oriented
+// programming means that the user is only presented with the essential features
+// and functionality of the website, while the implementation details are
+// hidden from them. This makes it easier for the user to interact with the
+// website without being overwhelmed by unnecessary information or
+// functionality.
+// - For example, imagine a website for a pizza delivery service. The user is
+// presented with a simple interface that allows them to select the type of
+// pizza they want, the toppings they want, and the delivery address. The user
+// does not need to know the details of how the website is implemented, such as
+// the database that stores the pizza orders, the algorithms that calculate the
+// delivery time, or the APIs that connect to the payment system. All of these
+// details are hidden from the user, and the user only needs to interact with
+// the essential features of the website.
+// - This abstraction also allows the website developers to change the
+// implementation details of the website without affecting the user experience.
+// For example, the developers may switch to a different database or payment
+// system, or they may optimise the algorithms for calculating delivery time.
+// These changes can be made without affecting the user interface, because the
+// user does not need to know about the implementation details.
+// - Overall, abstraction in object-oriented programming allows for a simpler,
+// more intuitive user experience, while also allowing for greater flexibility
+// and maintainability of the website's code.
+
+// A real-life analogy for abstraction in programming could be a car dashboard.
+// When driving a car, you do not need to know how the engine works or how the
+// fuel is injected into the cylinders. All you need to know is the information
+// displayed on the dashboard such as the speedometer, fuel gauge, temperature
+// gauge, etc. The dashboard abstracts the complexities of the car's internal
+// systems and presents only the essential information that is required for the
+// driver to operate the vehicle.
